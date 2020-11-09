287e6d3e6560f0d7ec095472c93e83c6114ea645
崮生 <admin@shenzilong.cn>
2020-11-09 15:57:24 +0800

    ✨feat(背景): 去除底部的碗状图片

--- src/component/background/index.js
+++ src/component/background/index.js
@@ -1,61 +1,61 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js'
+import Particles from 'react-particles-js';
 import logo from './bg.jpeg';
 import './background.css';
 const particles = {
-    number: {
-      value: 100,
-      density: {
-        enable: false,
-        value_area: 3200
-      }
+  number: {
+    value: 100,
+    density: {
+      enable: false,
+      value_area: 3200
+    }
+  },
+  color: {
+    value: "#0FF"
+  },
+  shape: {
+    type: "circle",
+    stroke: {
+      width: 0,
+      color: "#000000"
     },
-    color: {
-      value: "#0FF"
+    polygon: {
+      nb_sides: 5
     },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#000000"
-      },
-      polygon: {
-        nb_sides: 5
-      },
-      image: {
-        src: "",
-        width: 100,
-        height: 100
-      }
-    },
-    opacity: {
-      value: 1,
-      random: true,
-      anim: {
-        enable: 1,
-        speed: 10,
-        opacity_min: .5,
-        sync: !1
-      }
-    },
-    size: {
-      value: 3,
-      random: true,
-      anim: {
-        enable: !1,
-        speed: 20,
-        size_min: 1,
-        sync: true
-      }
-    },
-    line_linked: {
-      enable: !0,
-      distance: 120,
-      color: "#FF0",
-      opacity: .6,
-      width: 1,
-    },
-  };
+    image: {
+      src: "",
+      width: 100,
+      height: 100
+    }
+  },
+  opacity: {
+    value: 1,
+    random: true,
+    anim: {
+      enable: 1,
+      speed: 10,
+      opacity_min: .5,
+      sync: !1
+    }
+  },
+  size: {
+    value: 3,
+    random: true,
+    anim: {
+      enable: !1,
+      speed: 20,
+      size_min: 1,
+      sync: true
+    }
+  },
+  line_linked: {
+    enable: !0,
+    distance: 120,
+    color: "#FF0",
+    opacity: .6,
+    width: 1,
+  },
+};
 class Background extends Component {
   render() {
     return (
@@ -69,11 +69,11 @@ class Background extends Component {
             width: '100%',
             height: '100%',
             background: '#1a1a1a',
-            backgroundImage: `url(${logo})`,
+            // backgroundImage: `url(${logo})`,
             backgroundSize: 'cover',
             pointerEvents: 'none',
           }}
-        /> }
+        />}
         {this.props.children}
       </div>
     );
